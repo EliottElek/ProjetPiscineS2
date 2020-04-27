2c6ed750fd4d20215f6b88cb701e0d6b883ad6cb
Subeshan <61588915+Subeshan09@users.noreply.github.com>
2020-04-27 15:30:20 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 ///Projet piscine de Capucine Plantecoste, Subeshan Koneswaran, Eliott Morcillo
 ///Projet de la semaine du 27/04/2020
 #include "graphe.h"
+#include "couleur.h"
+#include"svgfile.h"
 
 
 int main()
