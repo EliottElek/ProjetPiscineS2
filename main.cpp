9cdaf615be1796d1d50d26b03e9f650461233167
EliottElek <64375473+EliottElek@users.noreply.github.com>
2020-04-27 10:43:41 +0200

    création d'un nouveau dossier

--- main.cpp
+++ main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
-
-using namespace std;
+#include<stdio.h>
+#include <fstream>
+#include <vector>
+#include <fstream>
+#include <sstream>
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    std::istringstream iss;
+    std::string strvalues ;
+    std::ifstream ifs("fichier1.txt");  // on ouvre en lecture
+    if (!ifs)
+    {
+        throw std::runtime_error {"Impossible d'ouvrir le fichier"};
+    }
+    std::string contenu;
+    getline(ifs, contenu);  // on met dans "contenu" la ligne
+    std::cout << contenu ;   // on affiche la ligne
+    iss.str (contenu);
+    for (i=0; i<20; i++)
+    {
+        ///lecture du fichier 
+    }
     return 0;
 }
