b8b1acbbb9ef9e1289cdc5953ce9f97c6079ccc0
Capucine Plantecoste <capucine.plantecoste@gmail.com>
2020-04-27 14:20:19 +0200

    graphe

---
+++ ProjetPiscineS2/graphe.cpp
@@ -0,0 +1,58 @@
+#include "graphe.h"
+
+Graphe :: Graphe(std::string nomFichier, std::string nomFichier2)
+{
+    int oriente, ordre, numsommet, x, y, taille, idarete, sommet1, sommet2, taille2, idarete2, poids ;
+    char idsommet;
+    std::istringstream iss;
+    std::string strvalues ;
+    std::ifstream ifs(nomFichier);  // on ouvre en lecture
+    std::ifstream ifs2 (nomFichier2);
+    if (!ifs)
+    {
+        throw std::runtime_error {"Impossible d'ouvrir le fichier"};
+    }
+    if (!ifs2)
+    {
+        throw std::runtime_error {"Impossible d'ouvrir le fichier"};
+    }
+    ifs >> oriente;
+    ifs >> ordre;
+    std::cout << "Ouverture des 2 fichiers:" << std::endl ;
+    std::cout << "Oriente:" << oriente << std::endl;
+    std::cout << "Ordre :" << ordre<< std::endl ;
+    for (int i=2; i<ordre+2; i++)
+    {
+        ifs >> numsommet;
+        std::cout << "Num sommet : " << numsommet;
+        ifs >> idsommet;
+        std::cout << " Idsommet : " << idsommet;
+        ifs >> x;
+        std::cout << " x : " << x;
+        ifs >> y ;
+        std::cout << " y : " << y << std::endl;
+    }
+    ifs >> taille;
+    std::cout << " Taille :" << taille << std::endl;
+    for (int j=ordre+4 ; j<ordre+4+taille; j++)
+    {
+        ifs >> idarete;
+        std::cout << "Idarete : " << idarete;
+        ifs >> sommet1;
+        std::cout << " Sommet1 : " << sommet1;
+        ifs >> sommet2;
+        std::cout << " Sommet2 : " << sommet2 << std::endl;
+    }
+
+    ifs2 >> taille2;
+    std::cout << std::endl;
+    for (int i=1; i<taille+1; i++)
+    {
+        ifs2 >> idarete2 ;
+        std :: cout << "Poids de l'arete " << idarete2 << " :" ;
+        ifs2 >> poids;
+        std::cout << poids << std::endl;
+
+    }
+
+}
--- ProjetPiscineS2/graphe.h
+++ ProjetPiscineS2/graphe.h
@@ -1,6 +1,18 @@
 #ifndef GRAPHE_H_INCLUDED
 #define GRAPHE_H_INCLUDED
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include<stdio.h>
+#include <vector>
 
+class Graphe
+{
+private :
+
+public :
+    Graphe(std::string nomFichier, std::string nomFichier2);
+};
 
 
 #endif // GRAPHE_H_INCLUDED
